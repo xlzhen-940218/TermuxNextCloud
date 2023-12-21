3382a548a5b8026c94ac6088b35799d1d361c070
xlzhen <xionglongzhen@gmail.com>
2023-12-22 00:15:06 +0800

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-cd ~/
-pkg upgrade -y \
-pkg install openssh -y \
-if [ -e ~/latest.zip ]; then
-    unzip ~/latest.zip 
+cd ~/;
+pkg upgrade -y;
+pkg install wget unzip openssh php -y;
+if [ ! -f ~/latest.zip ]; then
+    echo "File not found, start download..."
+    pkg install wget -y && wget https://download.nextcloud.com/server/releases/latest.zip
 else
-    pkg install wget -y && wget https://download.nextcloud.com/server/releases/latest.zip 
-    unzip ~/latest.zip 
+    echo "File exist,unzip..."
+    unzip ~/latest.zip
 fi
-pkg install php -y \
-echo \"cd ~/nextcloud && php -S 0.0.0.0:8888\" > start_server.sh \
-chmod 777 start_server.sh \
-echo \"sshd \r\n nohup ~/start_server.sh > /dev/null 2>&1 &\" > ~/.bashrc \
-exit
+echo "create start_server.sh file";
+echo "cd ~/nextcloud && php -S 0.0.0.0:8888" > start_server.sh;
+echo "set admin privigate permission to start_server.sh";
+chmod 777 start_server.sh;
+echo "set nextcloud php server init,please access link http://localhost:8888";
+echo "sshd \nnohup ~/start_server.sh > /dev/null 2>&1 &" > ~/.bashrc;
+echo "start server...";
+nohup ~/start_server.sh > /dev/null 2>&1 &
