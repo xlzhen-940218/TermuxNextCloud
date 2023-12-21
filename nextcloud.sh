da10d6cc8db1d5fb849b162a52423dc30cb2dd41
xlzhen <xionglongzhen@gmail.com>
2023-12-21 23:36:39 +0800

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -1,7 +1,8 @@
+cd ~/
 pkg upgrade -y \
 pkg install openssh -y \
 pkg install wget -y && wget https://download.nextcloud.com/server/releases/latest.zip \
-unzip latest.zip \
+unzip ~/latest.zip \
 pkg install php -y \
 echo \"cd ~/nextcloud && php -S 0.0.0.0:8888\" > start_server.sh \
 chmod 777 start_server.sh \
